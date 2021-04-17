2b1662915efc86d8240d826219c59b7e8464f3dc
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-04-16 20:28:24 -0700

    thought route and thought controller for adding, deleting, editing thoughts and reactions

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,9 +1,28 @@
-const { User, Thought, Reaction } = require('../models');
 
-const thoughtController = {
- // /api/thought route
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
 
-}
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
 
-module.exports = thoughtController;
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
 
+router.route('/:thoughtId/reactions/')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
